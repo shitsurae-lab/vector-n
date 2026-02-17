7be230d1d32543420b4442381c7a9d7e29f66674
shitsurae-lab <mahalo.morganite33@gmail.com>
2026-02-17 19:49:48 +0900

    feat: パンくずリストの実装

---
+++ components/Breadcrumbs/index.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import he from 'he';
+import { NAV_PATHS } from '@/app/constants/config';
+
+type Props = {
+  category?: string;
+  title?: string;
+};
+
+export const Breadcrumbs = ({ category, title }: Props) => {
+  return (
+    <nav className='flex gap-2 text-sm text-gray-500 mb-6'>
+      <Link href='/' className='hover:underline'>
+        HOME
+      </Link>
+
+      <span>/</span>
+      <Link href={`/${NAV_PATHS.WORKS.path}`} className='hover:underline'>
+        {NAV_PATHS.WORKS.label}
+      </Link>
+
+      {category && (
+        <>
+          <span>/</span>
+          <Link
+            href={`/${NAV_PATHS.WORKS.path}/${category}`}
+            className='hover:underline capitalize'
+          >
+            {category}
+          </Link>
+        </>
+      )}
+
+      {title && (
+        <>
+          <span>/</span>
+          <span className='text-gray-900 truncate'>{he.decode(title)}</span>
+        </>
+      )}
+    </nav>
+  );
+};
