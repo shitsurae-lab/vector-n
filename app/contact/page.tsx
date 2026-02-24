46d748e9d5609525d3dacb87a4c826ad15b498a8
shitsurae-lab <mahalo.morganite33@gmail.com>
2026-02-24 12:21:49 +0900

    feat: 見出し周り変更 - font-family変更（タイトル） - タイトル下、テキストの配置をspan(inline-block)に変更

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,14 +10,15 @@ export default function ContactPage() {
           <span className='text-[10px] font-bold tracking-[0.4em] uppercase text-zinc-400 mb-2'>
             Ready to start a project?
           </span>
-          <h1 className='text-center text-4xl md:text-5xl font-black tracking-[0.2em] mb-8 text-gray-800 uppercase'>
+          <h1 className='font-[family-name:var(--font-michroma)] text-center font-bold text-2xl md:text-3xl tracking-[0.5em] text-zinc-800 uppercase mb-8 md:mb-12'>
             Contact
           </h1>
         </div>
-        <p className='text-base md:text-lg font-medium text-zinc-800 leading-relaxed tracking-tight'>
-          制作のご依頼、採用に関するご相談など、
-          <br className='hidden md:block' />
-          お気軽にお問い合わせください。
+        <p className='flex flex-col text-base md:text-lg font-medium text-zinc-800 leading-relaxed tracking-tight'>
+          <span className='inline-block'>
+            制作のご依頼、採用に関するご相談など、
+          </span>
+          <span className='inline-block'> お気軽にお問い合わせください。</span>
         </p>
       </section>
       <div className='w-screen left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] relative'>
